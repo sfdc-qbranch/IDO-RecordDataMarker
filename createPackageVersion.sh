1492387e4f89c3bc8c4db27e222dd11420e49e74
Kevin <kakermanis@gmail.com>
2020-06-30 15:59:48 +1000

    package version created and general config clean up

--- createPackageVersion.sh
+++ createPackageVersion.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 clear
-DXPACKAGENAME="IDO <PACKAGE NAME>"
+DXPACKAGENAME="IDO Record Data Marker"
 
-read -p 'Use Snapshot? [y/n]: ' USESNAPSHOT
+#read -p 'Use Snapshot? [y/n]: ' USESNAPSHOT
 
 
 if [ "$USESNAPSHOT" == 'y' ]
 then
   echo 'Using a snapshot to create the package version'
-  sfdx force:package:version:create  --installationkeybypass --definitionfile config/snapshot-scratch-def.json --wait 10 --path force-app --package $DXPACKAGENAME $@
+  sfdx force:package:version:create  --installationkeybypass --definitionfile config/snapshot-scratch-def.json --wait 10 --path force-app --package "$DXPACKAGENAME" $@
 else
   echo 'Creating package version using a clean scratch org'
-  sfdx force:package:version:create  --installationkeybypass --definitionfile config/project-scratch-def.json --wait 10 --path force-app --package $DXPACKAGENAME" $@
+  sfdx force:package:version:create  --installationkeybypass --definitionfile config/project-scratch-def.json --wait 10 --path force-app --package "$DXPACKAGENAME" $@
 fi
